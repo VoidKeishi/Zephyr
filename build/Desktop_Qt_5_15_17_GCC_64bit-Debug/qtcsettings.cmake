b9d6b581e4abb9c8f4f57c10c785bde22ddfab12
Void <phananha4@gmail.com>
2024-06-12 23:04:45 +0700

    Initialize project

---
+++ build/Desktop_Qt_5_15_17_GCC_64bit-Debug/qtcsettings.cmake
@@ -0,0 +1,10 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "/home/void-keishi/Code/Zephyr/build/Desktop_Qt_5_15_17_GCC_64bit-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "/home/void-keishi/Qt/5.15.17/gcc_64/bin/qmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "/home/void-keishi/Qt/5.15.17/gcc_64" CACHE "PATH" "" FORCE)
+set("CMAKE_C_COMPILER" "/usr/bin/x86_64-linux-gnu-gcc-13" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "/usr/bin/g++-13" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
